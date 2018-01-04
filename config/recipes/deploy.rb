d3565507e4c4f472f79dfc23eb70df40c60821dc
Garrard Kitchen <garrard.kitchen@qcoach.com>
2018-01-04 12:29:35 +0000

    Update deploy.rb

--- config/recipes/deploy.rb
+++ config/recipes/deploy.rb
@@ -10,14 +10,22 @@ file "c:\\inetpub\\wwwroot\\status.html" do
   action  :create
 end
 
-template "c:\\inetpub\\wwwroot\\index.html" do
-  source "index.erb"
-  variables(
-    time: time,
-    instance_id: node["opsworks"]["instance"]["aws_instance_id"],
-    id: node["opsworks"]["instance"]["id"],
-    ip: node["opsworks"]["instance"]["ip"],
-    az: node["opsworks"]["instance"]["availability_zone"],
-    host_name: node["opsworks"]["instance"]["hostname"]
-    )
-end
+windowsserver = search(:aws_opsworks_instance, "hostname:web*").first
+Chef::Log.info("**********The public IP address is: '#{windowsserver[:public_ip]}'**********")
+Chef::Log.info("**********The private IP address is: '#{windowsserver[:private_ip]}'**********")
+Chef::Log.info("**********The ec2_instance_id is: '#{windowsserver[:ec2_instance_id]}'**********")
+Chef::Log.info("**********The availability_zone is: '#{windowsserver[:availability_zone]}'**********")
+Chef::Log.info("**********The hostname is: '#{windowsserver[:hostname]}'**********")
+Chef::Log.info("**********The time is: '#{time}'**********")
+
+# template "c:\\inetpub\\wwwroot\\index.html" do
+#   source "index.erb"
+#   variables(
+#     time: time,
+#     instance_id: node["opsworks"]["instance"]["aws_instance_id"],
+#     id: node["opsworks"]["instance"]["id"],
+#     ip: node["opsworks"]["instance"]["ip"],
+#     az: node["opsworks"]["instance"]["availability_zone"],
+#     host_name: node["opsworks"]["instance"]["hostname"]
+#     )
+# end
