96d2e640039e209de715f5f03605226644204df1
Garrard Kitchen <garrard.kitchen@qcoach.com>
2018-01-04 18:23:01 +0000

    Update deploy.rb

--- config/recipes/deploy.rb
+++ config/recipes/deploy.rb
@@ -36,34 +36,8 @@ chef_gem "aws-sdk" do
   action :install
 end
 
-# ruby_block "download-object" do
-#   block do
-#     require 'aws-sdk'
-
-#     #1
-#     Aws.config[:ssl_ca_bundle] = 'C:\ProgramData\Git\bin\curl-ca-bundle.crt'
-
-#     #2
-#     query = Chef::Search::Query.new
-#     app = query.search(:aws_opsworks_app, "type:other").first
-#     s3region = "US-EAST-1"
-#     s3bucket = "deleteme-evaluate-releases"
-#     s3filename = "20171214.1.zip"
-
-#     #3
-#     s3_client = Aws::S3::Client.new(region: s3region)
-#     s3_client.get_object(bucket: s3bucket,
-#                          key: s3filename,
-#                          response_target: 'C:\temp\evaluate.zip')
-#   end
-#   action :run
-# end
-
-
-
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
-
 uri = URI.parse(app["app_source"]["url"])
 uri_path_components = uri.path.split("/").reject{ |p| p.empty? }
 virtual_host_match = uri.host.match(/\A(.+)\.s3(?:[-.](?:ap|eu|sa|us)-(?:.+-)\d|-external-1)?\.amazonaws\.com/i)
@@ -78,28 +52,80 @@ else
 end
 
 s3_remote_path = uri_path_components.join("/")
-s3_base_uri.to_s.chomp!("/")
-
+Chef::Log.info("**********The uri is: '#{uri}'**********")
+Chef::Log.info("**********The s3_remote_path is: '#{s3_remote_path}'**********")
 ruby_block "download-object" do
   block do
     require 'aws-sdk'
-#application app_path do
-  
-    tmpdir = Dir.mktmpdir("temp")
-    directory tmpdir do
-      action :create
-    end
 
-    aws_s3_file "#{tmpdir}/archive" do
+    #1
+#    Aws.config[:ssl_ca_bundle] = 'C:\ProgramData\Git\bin\curl-ca-bundle.crt'
+
+    #2
+#    query = Chef::Search::Query.new
+#    app = query.search(:aws_opsworks_app, "type:other").first
+#     s3region = "US-EAST-1"
+#     s3bucket = "deleteme-evaluate-releases"
+#     s3filename = "20171214.1.zip"
+
+    #3
+#     s3_client = Aws::S3::Client.new(region: s3region)
+#     s3_client.get_object(bucket: s3bucket,
+#                          key: s3filename,
+#                          response_target: 'C:\temp\evaluate.zip')
+    
+    aws_s3_file "temp/evaluate.zip" do
       bucket s3_bucket
       remote_path s3_remote_path
       retries 3
     end
+    
+  end
+  action :run
+end
 
-    zipfile "#{tmpdir}/archive" do
-      into "#{app_path}"
-      overwrite true
-    end
+
+
+#app = search(:aws_opsworks_app).first
+#app_path = "/srv/#{app['shortname']}"
+
+# uri = URI.parse(app["app_source"]["url"])
+# uri_path_components = uri.path.split("/").reject{ |p| p.empty? }
+# virtual_host_match = uri.host.match(/\A(.+)\.s3(?:[-.](?:ap|eu|sa|us)-(?:.+-)\d|-external-1)?\.amazonaws\.com/i)
+# s3_base_uri = uri.dup
+
+# if virtual_host_match
+#   s3_bucket = virtual_host_match[1]
+#   s3_base_uri.path = "/"
+# else
+#   s3_bucket = uri_path_components[0]
+#   s3_base_uri.path = "/#{uri_path_components.shift}"
+# end
+
+# s3_remote_path = uri_path_components.join("/")
+# s3_base_uri.to_s.chomp!("/")
+
+# tmpdir = Dir.mktmpdir("temp")
+# directory tmpdir do
+#   action :create
+# end
+
+#ruby_block "download-object" do
+#  block do
+#    require 'aws-sdk'
+#application app_path do
+  
+   
+#     aws_s3_file "#{tmpdir}/archive" do
+#       bucket s3_bucket
+#       remote_path s3_remote_path
+#       retries 3
+#     end
+
+#     zipfile "#{tmpdir}/archive" do
+#       into "#{app_path}"
+#       overwrite true
+#     end
 
 #   link "#{app_path}/server.js" do
 #     to "#{app_path}/index.js"
@@ -109,6 +135,6 @@ ruby_block "download-object" do
 #   npm_start do
 #     action [:stop, :enable, :start]
 #   end
-  end
-  action :run
-end
+#  end
+#  action :run
+#end
