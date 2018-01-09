27c4bb747188828cce747b6f5a72cab4c9ab89e8
Garrard Kitchen <garrard.kitchen@qcoach.com>
2018-01-09 16:34:48 +0000

    Create setup.rb

---
+++ database/recipes/setup.rb
@@ -0,0 +1,11 @@
+cookbook_file 'c:/temp/db/install-mssqlodbc-driver.ps1' do
+  source 'install-mssqlodbc-driver.ps1'  
+  action :create
+end
+
+powershell_script 'install ms sql odbc driver' do
+  cwd "c:/temp/db"
+  code <<-EOH  
+  .\install-mssqlodbc-driver.ps1
+  EOH
+end
