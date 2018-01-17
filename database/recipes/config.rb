2afc192abd4d92399ab625a6c3c93584e2bb4c9e
Garrard Kitchen <garrard.kitchen@qcoach.com>
2018-01-17 17:14:58 +0000

    Update config.rb

--- database/recipes/config.rb
+++ database/recipes/config.rb
@@ -1,24 +1,41 @@
-Chef::Log.info("********** STAGE 1 **********")
+Chef::Log.info("********** CONFIG: START **********")
 
 chef_gem "aws-sdk" do
   compile_time false
   action :install
 end
 
-ssm = Aws::SSM::Client.new(
-  region: "us-east-1"
-)
+Chef::Log.info("********** CONFIG: STAGE 1 **********")
 
-resp = ssm.get_parameter({
-  name: "evaluate-mssql-dev-connstring",
-  with_decryption: true,
-})
+ruby_block "test ssm parameter" do
+    block do
+      require 'aws-sdk'
+    
+      Chef::Log.info("********** CONFIG: STAGE 2 **********")
+      
+      #1
+      Aws.config[:ssl_ca_bundle] = 'C:\ProgramData\Git\bin\curl-ca-bundle.crt'
 
-Chef::Log.info("********** STAGE 2 **********")
+      Chef::Log.info("********** CONFIG: STAGE 3 **********")
+      
+      #2
+      ssm = Aws::SSM::Client.new(region: "us-east-1")
 
-Chef::Log.info("********** OUTPUT **********")
-Chef::Log.info("  ssm name  = #{resp.parameter.name} ")
-Chef::Log.info("  ssm value = #{resp.parameter.value} ")
+      Chef::Log.info("********** CONFIG: STAGE 4 **********")
+      
+      #3
+      resp = ssm.get_parameter({
+        name: "evaluate-mssql-dev-connstring",
+        with_decryption: true
+      })
+      
+      Chef::Log.info("********** OUTPUT **********")
+      Chef::Log.info("  ssm name  = #{resp.parameter.name} ")
+      Chef::Log.info("  ssm value = #{resp.parameter.value} ")
 
-Chef::Log.info("********** END **********")
+    end
+    action :run
+  end
+
+Chef::Log.info("********** CONFIG: END **********")
 
