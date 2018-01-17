15e6ed97cbd131fb1ee8d56fb8346b9f871d4f9d
Garrard Kitchen <garrard.kitchen@qcoach.com>
2018-01-17 16:43:30 +0000

    Update setup.rb

--- database/recipes/setup.rb
+++ database/recipes/setup.rb
@@ -1,9 +1,15 @@
+Chef::Log.info("********** STAGE 1 **********")
+
 cookbook_file 'c:/temp/db/install-mssqlodbc-driver.ps1' do
   source 'install-mssqlodbc-driver.ps1'  
   action :create
 end
 
+Chef::Log.info("********** STAGE 2 **********")
+
 powershell_script 'install ms sql odbc driver' do
   cwd "c:/temp/db"
-  code ". install-mssqlodbc-driver.ps1"
+  code ". c:\temp\db\install-mssqlodbc-driver.ps1"
 end
+
+Chef::Log.info("********** END **********")
