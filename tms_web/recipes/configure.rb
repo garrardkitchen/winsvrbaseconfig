01dce318910a7341c3d4d56d282719a5a8cc8553
Garrard Kitchen <garrard.kitchen@qualtrak.com>
2018-01-25 15:39:54 +0000

    Reintroduced PS stop action

--- tms_web/recipes/configure.rb
+++ tms_web/recipes/configure.rb
@@ -7,7 +7,7 @@ Chef::Log.info("List of seeds for #{APP_NAME} = #{SEEDS}")
 
 powershell_script 'Update Seed IPs' do
     cwd "c:/temp/#{APP_NAME}"
-    code ". c:/temp/#{APP_NAME}/deploy/Patch-WebAkka.ps1 -Web TMS -SeedIPs #{SEEDS}"        
+    code ". c:/temp/#{APP_NAME}/deploy/Patch-WebAkka.ps1 -Web TMS -SeedIPs #{SEEDS} -ErrorAction Stop"        
 end
 
 Chef::Log.info("** TMS WEB: CONFIGURE END                        **")
